1d28a1aa3e8eba06c2b83bf4e0e3c605f1857f75
vesa780 <ravesa38@gmail.com>
2026-04-26 14:51:11 +0000

    Sun, Apr 26, 2026, 4:51 PM +02:00

--- src/05_command_line.c
+++ src/05_command_line.c
@@ -41,3 +41,52 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+TASK 5: Command Line
+Goal: Use argc/argv.
+*/
+
+int main(int argc, char *argv[]) {
+    // TODO 1: check argc (Program name + op + num1 + num2 = 4)
+    if (argc != 4) {
+        printf("Usage: %s <operation> <num1> <num2>\n", argv[0]);
+        printf("Operations: add, sub, mul, div\n");
+        return 1; // Exit with error code
+    }
+
+    // TODO 2: parse operation and numbers
+    char *op = argv[1];
+    long a = strtol(argv[2], NULL, 10);
+    long b = strtol(argv[3], NULL, 10);
+
+    // TODO 3-5: implement operations
+    if (strcmp(op, "add") == 0) {
+        printf("Result: %ld\n", a + b);
+    }
+    else if (strcmp(op, "sub") == 0) {
+        printf("Result: %ld\n", a - b);
+    }
+    else if (strcmp(op, "mul") == 0) {
+        printf("Result: %ld\n", a * b);
+    }
+    else if (strcmp(op, "div") == 0) {
+        // Handle division by zero
+        if (b == 0) {
+            printf("Error: Division by zero is not allowed.\n");
+            return 1;
+        }
+        printf("Result: %ld\n", a / b);
+    }
+    else {
+        // TODO 5: Unknown operation
+        printf("Unknown operation: %s\n", op);
+        return 1;
+    }
+
+    return 0;
+}
