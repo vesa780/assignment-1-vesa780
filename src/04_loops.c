822d11ac29ec0c8cf8b2c2cdc520ce9c8dec209f
vesa780 <ravesa38@gmail.com>
2026-04-26 14:49:59 +0000

    Sun, Apr 26, 2026, 4:49 PM +02:00

--- src/04_loops.c
+++ src/04_loops.c
@@ -42,3 +42,50 @@ int main(void) {
 
     return 0;
 }
+
+#include <stdio.h>
+
+/*
+TASK 4: Loops
+Goal: for + while + simple algorithms.
+*/
+
+int main(void) {
+    int n = 0;
+
+    // TODO 1: keep asking until n > 0
+    while (n <= 0) {
+        printf("Enter a positive integer n (> 0): ");
+        scanf("%d", &n);
+
+        if (n <= 0) {
+            printf("Invalid input. Please try again.\n");
+        }
+    }
+
+    printf("\n=== Counting ===\n");
+    // TODO 2: print 1..n
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    printf("\n=== Sum 1..n ===\n");
+    // TODO 3: compute sum
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i; // This is the same as sum = sum + i
+    }
+    printf("The sum of numbers from 1 to %d is: %d\n", n, sum);
+
+    printf("\n=== Factorial n! ===\n");
+    // TODO 4: compute factorial
+    unsigned long long factorial = 1;
+    for (int i = 1; i <= n; i++) {
+        factorial *= i; // This is the same as factorial = factorial * i
+    }
+    // Use %llu to print unsigned long long
+    printf("%d! = %llu\n", n, factorial);
+
+    return 0;
+}
